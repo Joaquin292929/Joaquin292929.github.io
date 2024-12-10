417eea826329e2f63752eefb26a0165f28824fc0
Joaquin292929 <161317609+Joaquin292929@users.noreply.github.com>
2024-12-10 09:23:00 +0100

    Organizacion codigo, formateo

--- js/password.js
+++ js/password.js
@@ -1,24 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Selecciona todos los botones de "Mostrar/Ocultar"
-    const toggleButtons = document.querySelectorAll('.toggle-password');
+document.addEventListener("DOMContentLoaded", function () {
+  // Selecciona todos los botones de "Mostrar/Ocultar"
+  const toggleButtons = document.querySelectorAll(".toggle-password");
 
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            // Encuentra el campo de contraseña asociado al botón
-            const passwordInput = button.previousElementSibling;
+  toggleButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Encuentra el campo de contraseña asociado al botón
+      const passwordInput = button.previousElementSibling;
 
-            // Verifica que el campo exista y alterna su visibilidad
-            if (passwordInput) {
-                if (passwordInput.type === 'password') {
-                    passwordInput.type = 'text';
-                    button.textContent = 'Ocultar';
-                } else {
-                    passwordInput.type = 'password';
-                    button.textContent = 'Mostrar';
-                }
-            } else {
-                console.error("Campo de contraseña no encontrado para el botón:", button);
-            }
-        });
+      // Verifica que el campo exista y alterna su visibilidad
+      if (passwordInput) {
+        if (passwordInput.type === "password") {
+          passwordInput.type = "text";
+          button.textContent = "Ocultar";
+        } else {
+          passwordInput.type = "password";
+          button.textContent = "Mostrar";
+        }
+      } else {
+        console.error(
+          "Campo de contraseña no encontrado para el botón:",
+          button
+        );
+      }
     });
+  });
 });
